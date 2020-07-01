a51adec273ebe3f98738b55265729c9f3a5b56d6
BuildTools <unconfigured@null.spigotmc.org>
2020-07-01 01:06:38 -0400

    built the frame, no logic yet

--- src/App.js
+++ src/App.js
@@ -1,26 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>React Calculator!</h1>
+			<div className="Calculator">
+				<Display />
+				<Clear />
+				<Keypad />
+				<Operations />
+				<Equal />
+			</div>
+		</div>
+	);
+}
+
+class Display extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+	render() {
+		return (
+			<div id="display">
+				<div id="top">0123456789</div>
+				<div id="bot">0123456789</div>
+			</div>
+		);
+	}
+}
+
+class Keypad extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	render() {
+		return (
+			<div className="Keypad">
+				<button id="seven">7</button>
+				<button id="eight">8</button>
+				<button id="nine">9</button>
+				<button id="four">4</button>
+				<button id="five">5</button>
+				<button id="six">6</button>
+				<button id="one">1</button>
+				<button id="two">2</button>
+				<button id="three">3</button>
+				<button id="zero">0</button>
+				<button id="decimal">.</button>
+			</div>
+		);
+	}
+}
+
+class Clear extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+	render() {
+		return <button id="clear">AC</button>;
+	}
+}
+
+class Operations extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+	render() {
+		return (
+			<React.Fragment>
+				<button id="add">&#43;</button>
+				<button id="subtract">&#8722;</button>
+				<button id="multiply">&#215;</button>
+				<button id="divide">&#247;</button>
+			</React.Fragment>
+		);
+	}
+}
+
+class Equal extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+	render() {
+		return <button id="equals">&#61;</button>;
+	}
 }
 
 export default App;
