95fb62e8ecbb38aa16bf3e95337ef702044682f3
BuildTools <unconfigured@null.spigotmc.org>
2020-07-09 02:39:14 -0400

    worked on inputs

--- src/App.js
+++ src/App.js
@@ -17,13 +17,32 @@ class Calculator extends Component {
 			display: "0",
 		};
 	}
+
+	handleOperator = (key) => this.setState({ display: key });
+
+	handleKey = (key) => {
+		this.setState({
+			display:
+				this.state.display === "0"
+					? this.state.display.replace("0", key)
+					: this.state.display.concat(key),
+		});
+	};
+
+	handleClearEntries = () => {
+		this.setState({
+			display: "0",
+		});
+	};
+
 	render() {
 		return (
 			<div className="Calculator">
-				<Display display={this.state.display} />
-				<Clear />
-				<Keypad />
-				<Operations />
+				<Display display={this.state.display} isConcat={this.state.isConcat} />
+				<GlobalClear />
+				<ClearEntry handleClearEntries={this.handleClearEntries} />
+				<Keypad handleKey={this.handleKey} />
+				<Operations handleOperator={this.handleOperator} />
 				<Equal />
 			</div>
 		);
@@ -33,12 +52,27 @@ class Calculator extends Component {
 class Display extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {
+			topDisplay: "",
+		};
 	}
+
+	componentDidUpdate(prevProps) {
+		if (this.props.display !== prevProps.display) {
+			const display = this.props.display;
+			this.setState({
+				topDisplay:
+					this.props.display === "0"
+						? ""
+						: this.state.topDisplay.concat(display[display.length - 1]),
+			});
+		}
+	}
+
 	render() {
 		return (
 			<div id="display">
-				<div id="top">0123456789</div>
+				<div id="top">{this.state.topDisplay}</div>
 				<div id="bot">{this.props.display}</div>
 			</div>
 		);
@@ -56,29 +90,65 @@ class Keypad extends Component {
 	render() {
 		return (
 			<div className="Keypad">
-				<button id="seven">7</button>
-				<button id="eight">8</button>
-				<button id="nine">9</button>
-				<button id="four">4</button>
-				<button id="five">5</button>
-				<button id="six">6</button>
-				<button id="one">1</button>
-				<button id="two">2</button>
-				<button id="three">3</button>
-				<button id="zero">0</button>
-				<button id="decimal">.</button>
+				<button id="seven" onClick={() => this.props.handleKey("7")}>
+					7
+				</button>
+				<button id="eight" onClick={() => this.props.handleKey("8")}>
+					8
+				</button>
+				<button id="nine" onClick={() => this.props.handleKey("9")}>
+					9
+				</button>
+				<button id="four" onClick={() => this.props.handleKey("4")}>
+					4
+				</button>
+				<button id="five" onClick={() => this.props.handleKey("5")}>
+					5
+				</button>
+				<button id="six" onClick={() => this.props.handleKey("6")}>
+					6
+				</button>
+				<button id="one" onClick={() => this.props.handleKey("1")}>
+					1
+				</button>
+				<button id="two" onClick={() => this.props.handleKey("2")}>
+					2
+				</button>
+				<button id="three" onClick={() => this.props.handleKey("3")}>
+					3
+				</button>
+				<button id="zero" onClick={() => this.props.handleKey("0")}>
+					0
+				</button>
+				<button id="decimal" onClick={() => this.props.handleKey(".")}>
+					.
+				</button>
 			</div>
 		);
 	}
 }
 
-class Clear extends Component {
+class GlobalClear extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {};
 	}
 	render() {
-		return <button id="clear">AC</button>;
+		return <button id="globalclear">C</button>;
+	}
+}
+
+class ClearEntry extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+	render() {
+		return (
+			<button id="clear" onClick={this.props.handleClearEntries}>
+				CE
+			</button>
+		);
 	}
 }
 
@@ -90,10 +160,18 @@ class Operations extends Component {
 	render() {
 		return (
 			<React.Fragment>
-				<button id="add">&#43;</button>
-				<button id="subtract">&#8722;</button>
-				<button id="multiply">&#215;</button>
-				<button id="divide">&#247;</button>
+				<button id="add" onClick={() => this.props.handleOperator("+")}>
+					&#43;
+				</button>
+				<button id="subtract" onClick={() => this.props.handleOperator("-")}>
+					&#8722;
+				</button>
+				<button id="multiply" onClick={() => this.props.handleOperator("*")}>
+					&#215;
+				</button>
+				<button id="divide" onClick={() => this.props.handleOperator("/")}>
+					&#247;
+				</button>
 			</React.Fragment>
 		);
 	}
